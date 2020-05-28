643ced4e3a93f49fdbd5f8087fed996f608896d1
SEVERIUG <sasha.severen@gmail.com>
2020-05-28 11:54:26 +0300

    Update parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -2,19 +2,21 @@
 #include <iostream>
 #include <cstring>
 #include <malloc.h> 
-void TrimRight(char *s)
+
+void TrimRight( char *s )
 {
  
     
-    if(!*s) return; // нет строки (нулевой указатель)
+    if (!*s) return; // нет строки (нулевой указатель)
     char* frs = s; // указатель на начало последней (самой правой) группы пробелов
-    while(*s){
-        if(*s != ' ')
-            frs = s+1; s++;}
+    while (*s){
+        if (*s != ' ')
+            frs = s + 1; s++;}
     *frs = '\0';      // задаем новый конец строки
-    
 }
-char *get_string(int *len) {
+
+char *get_string( int *len ) 
+{
     int size = 1000;
     *len = 0; // изначально строка пуста
     int capacity = 1; // ёмкость контейнера динамической строки (1, так как точно будет '\0')
@@ -31,16 +33,13 @@ char *get_string(int *len) {
             capacity *= 2; // увеличиваем ёмкость строки в два раза
             s = (char*) realloc(s, capacity * sizeof(char)); // создаём новую строку с увеличенной ёмкостью  
         }
-
         c = getchar(); // считываем следующий символ          
     }
-
     s[*len] = '\0'; // завершаем строку символом конца строки
-
     return s; // возвращаем указатель на считанную строку
 }
 
-int main(void)
+int main( void )
 {
    int len;
    char *s = get_string(&len);
